561179ab963c1384bb1047e72ae05145cbb058e6
gorbadil <ersin.mutlu@outlook.com.tr>
2023-05-22 02:04:24 +0300

    add model and arrange .venv files

--- ideas/views.py
+++ ideas/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse
+from ideas.models import Idea
+
 
 # Create your views here.
+
+def index(req):
+    idea = Idea.objects.order_by("created_at")
+    output = []
+    for i in idea:
+        output.append({
+            "title": i.title,
+            "desc": i.description,
+            "date": i.created_at,
+            "user": i.user
+        })
+    # output = {"title": ", ".join([i.title for i in idea]),
+    #           "desc": ", ".join([i.description for i in idea]),
+    #           "created_at": ([i.created_at for i in idea]),
+    #           "user": ", ".join([i.user for i in idea]),
+    #           }
+    return HttpResponse(output)
