2728c5233dcbfca8229afd989d57f6e826388e52
gorbadil <ersin.mutlu@outlook.com.tr>
2023-05-31 01:02:48 +0300

    delete, localstorage done. need edit

--- ideas/urls.py
+++ ideas/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("allideas/", views.allideas, name="allideas"),
-    path("addidea/", views.add_idea, name="addidea")
+    path("addidea/", views.add_idea, name="addidea"),
+    path("deleteidea/<int:id>/", views.delete_idea, name="deleteidea"),
 ]
