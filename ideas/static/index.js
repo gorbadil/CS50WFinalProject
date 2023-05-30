5ab0871cd4de5b19bef349e948038470f4e14cd3
gorbadil <ersin.mutlu@outlook.com.tr>
2023-05-31 02:03:02 +0300

    edit done. can publish now

--- ideas/static/index.js
+++ ideas/static/index.js
@@ -26,14 +26,22 @@ document.addEventListener("DOMContentLoaded", async () => {
   data.forEach((j) => {
     let username = localStorage.getItem("username");
     if (j.fields.user == username) {
+      let editbutton = document.createElement("button");
       let deletebutton = document.createElement("button");
       deletebutton.className = "deletebutton";
       deletebutton.innerHTML = "X";
+      editbutton.className = "editbutton";
+      editbutton.innerHTML = "✏️";
       let ideaid = document.querySelector(`#id${j.pk} h5`);
       deletebutton.className += ` ${j.pk}`;
+      editbutton.className += ` ${j.pk}`;
+      ideaid.appendChild(editbutton);
       ideaid.appendChild(deletebutton);
       deletebutton.addEventListener("click", (e) => {
         deleteIdea(e.target.classList[1]);
+      });
+      editbutton.addEventListener("click", (e) => {
+        editIdea(e.target.classList[1]);
       });
     }
   });
@@ -64,7 +72,6 @@ addbtn.addEventListener("click", () => {
   formdiv.addEventListener("click", (event) => {
     let setname = document.querySelector("#username");
     localStorage.setItem("username", setname.value);
-    // console.log(event)
     if (event.target.className == "addform") {
       formdiv.style.display = "none";
     }
@@ -82,4 +89,53 @@ const deleteIdea = (id) => {
     headers: { "X-CSRFToken": `${csrf}` },
   });
   location.reload();
+};
+
+const editDone = (id) => {
+  const csrf = document.cookie
+    .split("; ")
+    .find((cookie) => cookie.startsWith("csrftoken="))
+    .split("=")[1];
+  console.log(csrf);
+  fetch(`editidea/${id}/`, {
+    method: "POST",
+    headers: { "X-CSRFToken": `${csrf}` },
+  });
+  location.reload();
+};
+
+const editIdea = (id) => {
+  fetch(`editidea/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      let formdiv = document.querySelector(".addform");
+      formdiv.style.display = "flex";
+      let form = document.querySelector("#addideaform");
+      let hidediv = document.querySelector(".hidediv");
+      form.style.display = "flex";
+      form.action = `editidea/${id}/`;
+      hidediv.innerHTML = `<div id="formdiv"
+      <div class="mb-3">
+      <label for="ideatitle" class="form-label">Idea Title</label>
+      <input type="text" value="${data.title}" name="title" class="form-control" id="ideatitle" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+      <label for="ideadesc" class="form-label">Idea Description</label>
+      <textarea type="text" name="description" class="form-control" id="ideadesc">${data.description}</textarea>
+      </div>
+      <div class="mb-3">
+      <label for="username" class="form-label">Username</label>
+      <input value="${data.user}" type="text" name="user" class="form-control" id="username" aria-describedby="emailHelp">
+      </div>
+      <button type="submit" id="submit-btn" class="btn btn-primary">Edit</button>
+    </div>`;
+      formdiv.addEventListener("click", (event) => {
+        let setname = document.querySelector("#username");
+        localStorage.setItem("username", setname.value);
+        if (event.target.className == "addform") {
+          formdiv.style.display = "none";
+        }
+      });
+    });
 };
