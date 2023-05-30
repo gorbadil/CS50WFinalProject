681e6abbde2b1515d3ddb1ce330e499d969f4429
gorbadil <ersin.mutlu@outlook.com.tr>
2023-05-30 04:27:08 +0300

    idea cards done. need to add localStorage, add idea, delete idea, edit idea

--- ideas/static/index.js
+++ ideas/static/index.js
@@ -9,23 +9,28 @@ const getIdeas = async () => {
 
 document.addEventListener("DOMContentLoaded", async () => {
   let data = await getIdeas();
-  console.log(data);
+  // console.log(data)
   data.forEach((i) => {
     let div = document.createElement("div");
     div.className = "col-lg-4 col-md-6 col-sm-12";
     div.innerHTML = `
-        <div class="card border-dark mb-3">
-            <div class="card-body">
-                <h5 class="card-title">${i.fields.title}</h5>
-                <p class="card-text">${i.fields.description}</p>
-                <p class="card-created card-link">${i.fields.created_at.slice(
-                  0,
-                  10
-                )}</p>
-                <p class="card-user card-link">${i.fields.user}</p>
-            </div>
-        </div>`;
+    <div class="idea-card mb-3">
+    <div class="card-body" id="id${i.pk}">
+    <h5 class="card-title">${i.fields.title}</h5>
+    <p class="card-text">${i.fields.description}</p>
+    <p class="card-created card-link">${i.fields.created_at.slice(0, 10)}</p>
+      <p class="card-user card-link">${i.fields.user}</p>
+      </div>
+      </div>`;
     mainDiv.appendChild(div);
+  });
+  data.forEach((j) => {
+    let deletebutton = document.createElement("button");
+    deletebutton.className = "deletebutton";
+    deletebutton.innerHTML = "X";
+    let ideaid = document.querySelector(`#id${j.pk} h5`);
+    deletebutton.className += ` id${j.pk}`;
+    ideaid.appendChild(deletebutton);
   });
 });
 
@@ -34,19 +39,14 @@ addbtn.addEventListener("click", () => {
   div.className = "addform";
   div.innerHTML = `<form id="addideaform">
     <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      <label for="ideatitle" class="form-label">Idea Title</label>
+      <input type="text" class="form-control" id="ideatitle" aria-describedby="emailHelp">
     </div>
     <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1">
+      <label for="ideadesc" class="form-label">Idea Description</label>
+      <textarea type="text" class="form-control" id="ideadesc"></textarea>
     </div>
-    <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" id="submit-btn" class="btn btn-primary">Submit</button>
   </form>`;
   document.body.appendChild(div);
   div.addEventListener("click", (event) => {
